ca6f310c0de6877e46766536d9fb3c3143c09a96
Magdiel <mreyesleal@gmail.com>
2018-11-01 13:22:11 -0400

    Initial Commit

---
+++ 01-iamrich/codes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-iamrich
+//
+//  Created by Magdiel Reyes Leal on 11/1/18.
+//  Copyright © 2018 5SecondsApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
