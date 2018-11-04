cb859a0833e138f2c8185161f835b3ffba783565
Magdiel <mreyesleal@gmail.com>
2018-11-04 10:33:42 -0500

    cambios a las acciones de los botones

--- 01-iamrich/codes/ViewController.swift
+++ 01-iamrich/codes/ViewController.swift
@@ -26,8 +26,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func ButtomPress(_ sender: UIButton) {
-        
-        let controller = UIAlertController(title: "SoyRico", message: """
+       
+    let controller = UIAlertController(title: "SoyRico", message: """
                Soy Rico,
                Me lo Merezco,
                Soy Bueno,
@@ -37,12 +37,17 @@ class ViewController: UIViewController {
         let action = UIAlertAction(title: "SoyRico", style: .default) { _ in
             self.UIView.backgroundColor = UIColor.blue
            self.LabelSoyRico.text = "Por Fin Soy Rico"
+            self.imageDiamond.image = UIImage(named: "incredible")
+            self.LabelSoyRico.textColor = UIColor.red
+        
         }
         controller.addAction(action)
         
         let action2 = UIAlertAction(title: "Reset", style: .destructive) { _ in
              self.UIView.backgroundColor = UIColor.purple
             self.LabelSoyRico.text = "SoyRico"
+            self.imageDiamond.image = UIImage(named: "ruby")
+            self.LabelSoyRico.textColor = UIColor.red
             print("he pulsado el botom reset")
         }
         
@@ -55,12 +60,13 @@ class ViewController: UIViewController {
         self.show(controller, sender: nil)
  
         
+       
         
-        /*self.LabelSoyRico.text = "Por Fin Soy Rico"
+     
+        self.LabelSoyRico.text = "Por Fin Soy Rico"
         self.LabelSoyRico.textColor = UIColor.white
         self.LabelSoyRico.font = UIFont.systemFont(ofSize: 50)
-        self.imageDiamond.image = UIImage(named: "incredible")
-       */
+        //self.imageDiamond.image = UIImage(named: "incredible")
     
 }
 
